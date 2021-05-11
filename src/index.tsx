6aac34b46fe69de8ac69c83133024bec86ac5f2d
Bruce Jenn <2252263040@qq.com>
2021-05-11 18:49:31 +0800

    updated router using hash

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import {BrowserRouter} from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import 'highlight.js/styles/github.css';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root')
 );
 
